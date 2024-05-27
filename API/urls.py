d7a8640b8ca011a2a0aec16d3093c207790da68f
Palash Dhavle <palashdhavle15@gmail.com>
2024-05-27 19:25:33 +0530

    feat: :sparkles: mapped URL to views

---
+++ API/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path, include
+from . import views  
+
+urlpatterns = [
+    path("category/",views.CategoryListCreate.as_view(), name='category-list-create'),
+    path("tasks/",views.TaskListCreate.as_view(), name='task-list-create'),
+    path("tasks/<str:pk>",views.TaskRetrieveUpdateDestroy.as_view(), name='task-retirve-update-destroy'),
+    path("goals/",views.GolasListCreate.as_view(), name='goal-list-create'),
+    path("goals/<str:pk>",views.GoalsRetrieveUpdateDestroy.as_view(), name='goal-retirve-update-destroy'),
+    path("signup/",views.UserCreate.as_view(), name='')
+]
