e3eb4828f18b5989e83cd3fd8dc12943147de359
Palash Dhavle <palashdhavle15@gmail.com>
2024-05-30 17:28:19 +0530

    feat: :sparkles: update User Serializer to support custom user
    Updated serializer to support the custom user model as well as add aditional field that ensure user inputs correct password

--- API/serializers.py
+++ API/serializers.py
@@ -1,6 +1,9 @@
 from rest_framework import serializers
 from .models import Tasks,Goals, Categories
 from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model
+from django.contrib.auth.password_validation import validate_password
+User = get_user_model()
 
 class UserSerializer(serializers.ModelSerializer):
     # since user has reverse relation to these fields it wont be added to the User Serializer by default so we add it 
@@ -11,12 +14,30 @@ class UserSerializer(serializers.ModelSerializer):
     # but the slug field column should be unique.
     # goals = serializers.SlugRelatedField(many=True, queryset=Tasks.objects.all(), slug_field='name')
     # tasks = serializers.SlugRelatedField(many=True, queryset=Tasks.objects.all(), slug_field='name')
-    
+    re_password = serializers.CharField(write_only=True, required=True)
     class Meta:
         model = User
         fields = [
-            'id','email','password',
+            'id','first_name','last_name','username','email','password','re_password'
         ]
+    
+    def validate(self, data):
+        # Call super to run default validations
+        data = super().validate(data)
+
+        # Validate password using Django's built-in password validators
+        password = data.get('password')
+        if password:
+            validate_password(password)
+
+        # Check if the password and re-entered password match
+        if password != data.get('re_password'):
+            raise serializers.ValidationError("The passwords do not match.")
+
+        # Pop out the re-entered password before returning the validated data
+        data.pop('re_password')
+
+        return data
 
 class CategorySerializers(serializers.ModelSerializer):
     # we are making the owner field readonly so no one can modify it via serializer data directly.
