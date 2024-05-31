c5ae7e33e9e9e71df1205b7f11a54f228d78ff2d
Palash Dhavle <palashdhavle15@gmail.com>
2024-05-31 20:24:25 +0530

    chore: :arrow_up: set URL routing for auth

--- API/urls.py
+++ API/urls.py
@@ -7,5 +7,7 @@ urlpatterns = [
     path("tasks/<str:pk>",views.TaskRetrieveUpdateDestroy.as_view(), name='task-retirve-update-destroy'),
     path("goals/",views.GolasListCreate.as_view(), name='goal-list-create'),
     path("goals/<str:pk>",views.GoalsRetrieveUpdateDestroy.as_view(), name='goal-retirve-update-destroy'),
-    path("signup/",views.UserCreate.as_view(), name='')
+    path("signup/",views.UserCreate.as_view(), name=''),
+    path('', include('rest_framework.urls'))
+
 ]
