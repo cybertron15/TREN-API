2cf9f7654c70705c43432f3d3a046fc020f13e4e
Palash Dhavle <palashdhavle15@gmail.com>
2024-06-14 22:53:32 +0530

    fix: :bug: fix setting config causing auth for every route by default

--- TREN_API/settings.py
+++ TREN_API/settings.py
@@ -131,9 +131,9 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTH_USER_MODEL = 'API.CustomUser'
 
 REST_FRAMEWORK = {
-    'DEFAULT_PERMISSION_CLASSES': (
-        'rest_framework.permissions.IsAuthenticated',
-    ),
+    # 'DEFAULT_PERMISSION_CLASSES': (
+    #     'rest_framework.permissions.IsAuthenticated',
+    # ),
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
