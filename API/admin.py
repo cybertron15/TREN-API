054e62739eedbd91b59a3f512d25c2a98f7be151
Palash Dhavle <palashdhavle15@gmail.com>
2024-06-15 21:45:34 +0530

    chore: :arrow_up: registed user model with admin

--- API/admin.py
+++ API/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Tasks
+from .models import Tasks, Categories
+from django.contrib.auth import get_user_model
 
-admin.site.register(Tasks)
+# getting custom user model
+User = get_user_model()
+
+admin.site.register([User,Tasks,Categories])
